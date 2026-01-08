3b7799c6b7495b612ead10d775ddb9970bfddbdb
Karim El-Hayawan <snackage@gmail.com>
2026-01-08 18:51:21 +0100

    fix vis

--- src/vis.c
+++ src/vis.c
@@ -11,22 +11,28 @@
 void set_block(int block_size, COLOR color, int x_pos, int y_pos) {
     printf("\x1b[s");
 
-    int x_shift = STEP_SIZE * x_pos + 1;
-    int y_shift = y_pos + 1;
-    printf("\x1b[%dG\x1b[%dB", x_shift, y_shift);
+    int x_shift = x_pos != 0 ? STEP_SIZE * x_pos + 1 : x_pos;
+    int y_shift = y_pos;
+    if(x_shift != 0) {
+        printf("\x1b[%dG", x_shift);
+    }
+    if(y_shift != 0) {
+        printf("\x1b[%dB", y_shift);
+    }
 
     for(int i = 0; i < block_size; ++i) {
         for(int j = 0; j < block_size; ++j) {
             printf("\x1b[38;5;%dm\x1b[48;5;%dm▀\x1b[0m", color, color);
             printf("\x1b[38;5;%dm\x1b[48;5;%dm▀\x1b[0m", color, color);
         }
-        printf("\x1b[1B\x1b[1G\x1b[%dG", x_shift);
+        printf("\x1b[1B\x1b[%dG", x_shift);
     }
     printf("\x1b[u");
 }
 
 void render_grid(int size) {
-    printf("\x1b[%dB\n", STEP_SIZE * (size + 1));
+    printf("\x1b[%dB", size);
+    fflush(stdout);
 }
 
 void prep_grid(int size) {
@@ -41,15 +47,15 @@ void prep_grid(int size) {
 #endif
     }
 
-    printf("\x1b[%dA", size + 1);
+    printf("\x1b[%dA", size);
 }
 
 void reset_grid(int size) {
     printf("\x1b[%dA", size);
 }
 
-int main(int argc, char** argv) {
-    int grid_size = 15;
+int main() {
+    int grid_size = 45;
 
     prep_grid(grid_size);
     set_block(2, GREEN, 1, 0);
@@ -57,8 +63,8 @@ int main(int argc, char** argv) {
     set_block(2, ROYAL_BLUE, 5, 2);
     set_block(5, GRAY, 5, 5);
     render_grid(grid_size);
-
     reset_grid(grid_size);
+
     usleep(25 * 100000);
 
     prep_grid(grid_size);
@@ -66,8 +72,8 @@ int main(int argc, char** argv) {
     set_block(1, WHITE, 0, 0);
     set_block(2, BLUE, 5, 2);
     render_grid(grid_size);
-
     reset_grid(grid_size);
+
     usleep(25 * 100000);
 
     prep_grid(grid_size);
